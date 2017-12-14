fe8ab14449919d15900290f8ac927085f0fa8703
beni <act.dev10@gmail.com>
2017-12-14 10:57:15 +0900

    [Epic02]프로젝트 리스트 조회 프로젝트명을 url에 붙여서 호출하면 하단의 새로운 테이블에 해당 프로젝트 코드를 가진 사람 목록이 보여짐

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -3,10 +3,19 @@
     <h1 class="pull-left">Project</h1>
     <div class="table">
       <vuetable ref="vuetable"
-                :api-url="callURL"
-                :fields="fields"
+                :api-url="projectListCallURL"
+                :fields="projectFields"
                 title="PROJECT"
+                @click="onRowClick"
       ></vuetable>
+        <br>
+        <h2>Result</h2>
+        <vuetable ref="vuetable"
+                  :api-url="projectSelectedURL"
+                  :fields="projectSelectedField"
+                  title="PROJECT SELECTED"
+        ></vuetable>
+
     </div>
   </div>
 </template>
@@ -17,9 +26,15 @@
 
     export default {
         name: 'project',
+        methods:{
+            onRowClick : function(row){
+                alert(1);
+            }
+
+        },
         data() {
             return {
-                fields: [{
+                projectFields: [{
                     name: 'id',
                     title: 'ID'
                 },
@@ -30,7 +45,8 @@
                     },
                     {
                         name: 'projectCode',
-                        title: '프로젝트코드'
+                        title: '프로젝트코드',
+                        onclick : 'searchProjectPerson'
                     },
                     {
                         name: 'totalCount',
@@ -48,7 +64,64 @@
                         name: 'cx',
                         title: 'CX'
                     }],
-                callURL: CONFIG.API_URL + "/project"
+                projectListCallURL: CONFIG.API_URL + "/project",
+
+                searchProjectCode : "PJT_0542",
+                projectSelectedField: [{
+                    name: 'id',
+                    title: 'ID'
+                },
+
+                    {
+                        name: 'name',
+                        title: '이름'
+                    }
+                    ,{
+                        name: 'nickname',
+                        title: '닉네임'
+                    }
+                    ,{
+                        name: 'email',
+                        title: '이메일'
+                    }
+
+                    ,{
+                        name: 'employeeNumber',
+                        title: '사번'
+                    },
+                    {
+                        name: 'cellPhoneNumber',
+                        title: '휴대전화번호'
+                    },
+                    {
+                        name: 'team',
+                        title: '팀명'
+                    },
+                    {
+                        name: 'department',
+                        title: '부서명'
+                    },
+                    {
+                        name: 'projectName',
+                        title: '프로젝트명'
+                    },
+                    {
+                        name: 'projectCode',
+                        title: '프로젝트코드'
+                    },
+                    {
+                        name: 'rank',
+                        title: '직급'
+                    }, {
+                        name: 'duty',
+                        title: '직무'
+                    },{
+                        name: 'assignType',
+                        title: '투입구분'
+                    }],
+                projectSelectedURL: CONFIG.API_URL + "/dashboard/projectCode/PJT_0542"
+
+
             }
         },
         components: {
