908b18cf2dd28abac5f1bb91972212a33fcddfaf
act-dev20 <Devcraft1!>
2017-12-14 11:09:44 +0900

    [User-001] Get teams with Team.vue

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,16 +1,59 @@
 <template>
+    <div>
+        <h1 class="pull-left">Team</h1>
+        <div class="table">
+            <vuetable ref="vuetable"
+                      :api-url="callURL"
+                      :fields="fields"
+                      title="Team"
+            ></vuetable>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Vuetable from 'vuetable-2/src/components/Vuetable'
+    import * as CONFIG from '../config'
 
   export default {
-    name: 'team',
+      name: 'team',
     data() {
       return {
+          fields: [{
+              name: 'id',
+              title: 'ID'
+          }
+              ,{
+                  name: 'team',
+                  title: '이름'
+              }
+              ,{
+                  name: 'department',
+                  title: '부서'
+              }
+              ,{
+                  name: 'totalCount',
+                  title: '총 인원수'
+              }
+
+              ,{
+                  name: 'earning',
+                  title: '가득'
+              },
+              {
+                  name: 'working',
+                  title: '가동'
+              },
+              {
+                  name: 'staying',
+                  title: '비가동'
+              }],
+          callURL: CONFIG.API_URL + "/teams"
       }
 
     },
     components: {
+        Vuetable
     }
   }
 </script>
